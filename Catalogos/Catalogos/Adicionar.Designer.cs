250245429d04b6982be6f61bbe3b23fd1c8c2c64
Pedro Coelho <pedro_junior37@hotmail.com>
2021-10-25 11:21:17 -0300

    Parte inicial

---
+++ Catalogos/Catalogos/Adicionar.Designer.cs
@@ -0,0 +1,360 @@
+﻿
+namespace Catalogos
+{
+    partial class Adicionar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdnFilme = new System.Windows.Forms.RadioButton();
+            this.rdnSerie = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ckbTerror = new System.Windows.Forms.CheckBox();
+            this.ckbSuspense = new System.Windows.Forms.CheckBox();
+            this.ckbRomance = new System.Windows.Forms.CheckBox();
+            this.ckbMusical = new System.Windows.Forms.CheckBox();
+            this.ckbFic = new System.Windows.Forms.CheckBox();
+            this.cknFantasia = new System.Windows.Forms.CheckBox();
+            this.ckbFaroeste = new System.Windows.Forms.CheckBox();
+            this.ckbEspionagem = new System.Windows.Forms.CheckBox();
+            this.ckbDrama = new System.Windows.Forms.CheckBox();
+            this.ckbDoc = new System.Windows.Forms.CheckBox();
+            this.ckbComedia = new System.Windows.Forms.CheckBox();
+            this.ckbAventura = new System.Windows.Forms.CheckBox();
+            this.ckbAcao = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitulo = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAno = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(29, 288);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(630, 23);
+            this.btnAdicionar.TabIndex = 0;
+            this.btnAdicionar.Text = "Adicionar";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.rdnFilme);
+            this.groupBox1.Controls.Add(this.rdnSerie);
+            this.groupBox1.Location = new System.Drawing.Point(29, 185);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.groupBox1.Size = new System.Drawing.Size(168, 60);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "filme ou Serie";
+            // 
+            // rdnFilme
+            // 
+            this.rdnFilme.AutoSize = true;
+            this.rdnFilme.Location = new System.Drawing.Point(94, 22);
+            this.rdnFilme.Name = "rdnFilme";
+            this.rdnFilme.Size = new System.Drawing.Size(54, 19);
+            this.rdnFilme.TabIndex = 1;
+            this.rdnFilme.TabStop = true;
+            this.rdnFilme.Text = "Filme";
+            this.rdnFilme.UseVisualStyleBackColor = true;
+            // 
+            // rdnSerie
+            // 
+            this.rdnSerie.AutoSize = true;
+            this.rdnSerie.Location = new System.Drawing.Point(17, 23);
+            this.rdnSerie.Name = "rdnSerie";
+            this.rdnSerie.Size = new System.Drawing.Size(50, 19);
+            this.rdnSerie.TabIndex = 0;
+            this.rdnSerie.TabStop = true;
+            this.rdnSerie.Text = "Serie";
+            this.rdnSerie.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ckbTerror);
+            this.groupBox2.Controls.Add(this.ckbSuspense);
+            this.groupBox2.Controls.Add(this.ckbRomance);
+            this.groupBox2.Controls.Add(this.ckbMusical);
+            this.groupBox2.Controls.Add(this.ckbFic);
+            this.groupBox2.Controls.Add(this.cknFantasia);
+            this.groupBox2.Controls.Add(this.ckbFaroeste);
+            this.groupBox2.Controls.Add(this.ckbEspionagem);
+            this.groupBox2.Controls.Add(this.ckbDrama);
+            this.groupBox2.Controls.Add(this.ckbDoc);
+            this.groupBox2.Controls.Add(this.ckbComedia);
+            this.groupBox2.Controls.Add(this.ckbAventura);
+            this.groupBox2.Controls.Add(this.ckbAcao);
+            this.groupBox2.Location = new System.Drawing.Point(219, 136);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(440, 146);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Categoria";
+            // 
+            // ckbTerror
+            // 
+            this.ckbTerror.AutoSize = true;
+            this.ckbTerror.Location = new System.Drawing.Point(17, 121);
+            this.ckbTerror.Name = "ckbTerror";
+            this.ckbTerror.Size = new System.Drawing.Size(56, 19);
+            this.ckbTerror.TabIndex = 12;
+            this.ckbTerror.Text = "Terror";
+            this.ckbTerror.UseVisualStyleBackColor = true;
+            // 
+            // ckbSuspense
+            // 
+            this.ckbSuspense.AutoSize = true;
+            this.ckbSuspense.Location = new System.Drawing.Point(301, 96);
+            this.ckbSuspense.Name = "ckbSuspense";
+            this.ckbSuspense.Size = new System.Drawing.Size(75, 19);
+            this.ckbSuspense.TabIndex = 11;
+            this.ckbSuspense.Text = "Suspense";
+            this.ckbSuspense.UseVisualStyleBackColor = true;
+            // 
+            // ckbRomance
+            // 
+            this.ckbRomance.AutoSize = true;
+            this.ckbRomance.Location = new System.Drawing.Point(175, 96);
+            this.ckbRomance.Name = "ckbRomance";
+            this.ckbRomance.Size = new System.Drawing.Size(76, 19);
+            this.ckbRomance.TabIndex = 10;
+            this.ckbRomance.Text = "Romance";
+            this.ckbRomance.UseVisualStyleBackColor = true;
+            // 
+            // ckbMusical
+            // 
+            this.ckbMusical.AutoSize = true;
+            this.ckbMusical.Location = new System.Drawing.Point(17, 96);
+            this.ckbMusical.Name = "ckbMusical";
+            this.ckbMusical.Size = new System.Drawing.Size(67, 19);
+            this.ckbMusical.TabIndex = 9;
+            this.ckbMusical.Text = "Musical";
+            this.ckbMusical.UseVisualStyleBackColor = true;
+            // 
+            // ckbFic
+            // 
+            this.ckbFic.AutoSize = true;
+            this.ckbFic.Location = new System.Drawing.Point(301, 72);
+            this.ckbFic.Name = "ckbFic";
+            this.ckbFic.Size = new System.Drawing.Size(113, 19);
+            this.ckbFic.TabIndex = 8;
+            this.ckbFic.Text = "Ficção Cientifica";
+            this.ckbFic.UseVisualStyleBackColor = true;
+            // 
+            // cknFantasia
+            // 
+            this.cknFantasia.AutoSize = true;
+            this.cknFantasia.Location = new System.Drawing.Point(175, 72);
+            this.cknFantasia.Name = "cknFantasia";
+            this.cknFantasia.Size = new System.Drawing.Size(69, 19);
+            this.cknFantasia.TabIndex = 7;
+            this.cknFantasia.Text = "Fantasia";
+            this.cknFantasia.UseVisualStyleBackColor = true;
+            // 
+            // ckbFaroeste
+            // 
+            this.ckbFaroeste.AutoSize = true;
+            this.ckbFaroeste.Location = new System.Drawing.Point(17, 72);
+            this.ckbFaroeste.Name = "ckbFaroeste";
+            this.ckbFaroeste.Size = new System.Drawing.Size(70, 19);
+            this.ckbFaroeste.TabIndex = 6;
+            this.ckbFaroeste.Text = "Faroeste";
+            this.ckbFaroeste.UseVisualStyleBackColor = true;
+            // 
+            // ckbEspionagem
+            // 
+            this.ckbEspionagem.AutoSize = true;
+            this.ckbEspionagem.Location = new System.Drawing.Point(301, 48);
+            this.ckbEspionagem.Name = "ckbEspionagem";
+            this.ckbEspionagem.Size = new System.Drawing.Size(91, 19);
+            this.ckbEspionagem.TabIndex = 5;
+            this.ckbEspionagem.Text = "Espionagem";
+            this.ckbEspionagem.UseVisualStyleBackColor = true;
+            // 
+            // ckbDrama
+            // 
+            this.ckbDrama.AutoSize = true;
+            this.ckbDrama.Location = new System.Drawing.Point(175, 48);
+            this.ckbDrama.Name = "ckbDrama";
+            this.ckbDrama.Size = new System.Drawing.Size(61, 19);
+            this.ckbDrama.TabIndex = 4;
+            this.ckbDrama.Text = "Drama";
+            this.ckbDrama.UseVisualStyleBackColor = true;
+            // 
+            // ckbDoc
+            // 
+            this.ckbDoc.AutoSize = true;
+            this.ckbDoc.Location = new System.Drawing.Point(17, 49);
+            this.ckbDoc.Name = "ckbDoc";
+            this.ckbDoc.Size = new System.Drawing.Size(102, 19);
+            this.ckbDoc.TabIndex = 3;
+            this.ckbDoc.Text = "Documentário";
+            this.ckbDoc.UseVisualStyleBackColor = true;
+            // 
+            // ckbComedia
+            // 
+            this.ckbComedia.AutoSize = true;
+            this.ckbComedia.Location = new System.Drawing.Point(301, 23);
+            this.ckbComedia.Name = "ckbComedia";
+            this.ckbComedia.Size = new System.Drawing.Size(74, 19);
+            this.ckbComedia.TabIndex = 2;
+            this.ckbComedia.Text = "Comédia";
+            this.ckbComedia.UseVisualStyleBackColor = true;
+            // 
+            // ckbAventura
+            // 
+            this.ckbAventura.AutoSize = true;
+            this.ckbAventura.Location = new System.Drawing.Point(175, 23);
+            this.ckbAventura.Name = "ckbAventura";
+            this.ckbAventura.Size = new System.Drawing.Size(74, 19);
+            this.ckbAventura.TabIndex = 1;
+            this.ckbAventura.Text = "Aventura";
+            this.ckbAventura.UseVisualStyleBackColor = true;
+            // 
+            // ckbAcao
+            // 
+            this.ckbAcao.AutoSize = true;
+            this.ckbAcao.Location = new System.Drawing.Point(17, 23);
+            this.ckbAcao.Name = "ckbAcao";
+            this.ckbAcao.Size = new System.Drawing.Size(53, 19);
+            this.ckbAcao.TabIndex = 0;
+            this.ckbAcao.Text = "Ação";
+            this.ckbAcao.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Titulo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Descrição";
+            // 
+            // txtTitulo
+            // 
+            this.txtTitulo.Location = new System.Drawing.Point(97, 13);
+            this.txtTitulo.Name = "txtTitulo";
+            this.txtTitulo.Size = new System.Drawing.Size(562, 23);
+            this.txtTitulo.TabIndex = 5;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(97, 43);
+            this.txtDescricao.Multiline = true;
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(562, 87);
+            this.txtDescricao.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(29, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ano";
+            // 
+            // txtAno
+            // 
+            this.txtAno.Location = new System.Drawing.Point(97, 136);
+            this.txtAno.Name = "txtAno";
+            this.txtAno.Size = new System.Drawing.Size(100, 23);
+            this.txtAno.TabIndex = 8;
+            // 
+            // Adicionar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(691, 323);
+            this.Controls.Add(this.txtAno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtTitulo);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnAdicionar);
+            this.Name = "Adicionar";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Adicionar";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdnFilme;
+        private System.Windows.Forms.RadioButton rdnSerie;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox ckbTerror;
+        private System.Windows.Forms.CheckBox ckbSuspense;
+        private System.Windows.Forms.CheckBox ckbRomance;
+        private System.Windows.Forms.CheckBox ckbMusical;
+        private System.Windows.Forms.CheckBox ckbFic;
+        private System.Windows.Forms.CheckBox cknFantasia;
+        private System.Windows.Forms.CheckBox ckbFaroeste;
+        private System.Windows.Forms.CheckBox ckbEspionagem;
+        private System.Windows.Forms.CheckBox ckbDrama;
+        private System.Windows.Forms.CheckBox ckbDoc;
+        private System.Windows.Forms.CheckBox ckbComedia;
+        private System.Windows.Forms.CheckBox ckbAventura;
+        private System.Windows.Forms.CheckBox ckbAcao;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitulo;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAno;
+    }
+}
---
+++ Catalogos/Catalogos/Inicial.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace Catalogos
+{
+    partial class Inicial
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(180, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bem vindo!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(122, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(246, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Escolha uma opção";
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(43, 105);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(170, 55);
+            this.btnIncluir.TabIndex = 2;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(253, 105);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(170, 55);
+            this.btnExcluir.TabIndex = 3;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(43, 184);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(170, 54);
+            this.btnAlterar.TabIndex = 4;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(253, 184);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(170, 54);
+            this.btnListar.TabIndex = 5;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // Inicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(450, 265);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Name = "Inicial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Luxflix";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnListar;
+    }
+}
+
